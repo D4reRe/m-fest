c8340ae39e550e5f9e09659dae3ffc68a52f764e
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-10-13 00:02:01 +0700

    feat: added temporary login & sign-up pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/app/(auth)/login/login";
+import React from "react";
+
+function LoginPage() {
+  return <LoginForm />;
+}
+
+export default LoginPage;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import SignUpForm from "@/app/(auth)/sign-up/sign-up";
+import React from "react";
+
+function SignUpPage() {
+  return <SignUpForm />;
+}
+
+export default SignUpPage;
