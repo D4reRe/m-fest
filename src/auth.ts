50dde23e3e0a65a2105da596919703876e46e493
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-10-14 10:11:56 +0700

    feat: change database to neon db

--- src/auth.ts
+++ src/auth.ts
@@ -1,30 +1,45 @@
 import NextAuth from "next-auth";
-import { SupabaseAdapter } from "@auth/supabase-adapter";
+// import { SupabaseAdapter } from "@auth/supabase-adapter";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-import jwt from "jsonwebtoken";
+import Resend from "next-auth/providers/resend";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "@neondatabase/serverless";
+// import jwt from "jsonwebtoken";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google, Github],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  }),
-  // callbacks: {
-  //   async session({ session, user }) {
-  //     const signingSecret = process.env.SUPABASE_JWT_SECRET;
-  //     if (signingSecret) {
-  //       const payload = {
-  //         aud: "authenticated",
-  //         exp: Math.floor(new Date(session.expires).getTime() / 1000),
-  //         sub: user.id,
-  //         email: user.email,
-  //         role: "authenticated",
-  //       };
-  //       session.supabaseAccessToken = jwt.sign(payload, signingSecret);
-  //     }
-  //     return session;
-  //   },
-  // },
-  secret: process.env.NEXTAUTH_SECRET,
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return {
+    adapter: PostgresAdapter(pool),
+    providers: [
+      Resend({ from: "Test <onboarding@resend.dev>" }),
+      Google,
+      Github,
+    ],
+  };
 });
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [Google, Github],
+//   adapter: SupabaseAdapter({
+//     url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+//   }),
+//   callbacks: {
+//     async session({ session, user }) {
+//       const signingSecret = process.env.SUPABASE_JWT_SECRET;
+//       if (signingSecret) {
+//         const payload = {
+//           aud: "authenticated",
+//           exp: Math.floor(new Date(session.expires).getTime() / 1000),
+//           sub: user.id,
+//           email: user.email,
+//           role: "authenticated",
+//         };
+//         session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+//       }
+//       return session;
+//     },
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
