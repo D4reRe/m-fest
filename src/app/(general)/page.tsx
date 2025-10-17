f0254e7f4549d51c153553a07508e79b45cef9a5
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-10-17 10:51:35 +0700

    feat: added sponsors

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -1,13 +1,14 @@
 import HeroSection from "@/app/(general)/_components/hero-section";
 import FAQs from "./_components/faqs";
-import EventsSection from "@/components/events";
 import Timeline from "@/components/timeline/timeline";
+import Sponsors from "@/components/sponsors";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <Timeline />
+      <Sponsors />
       <FAQs />
     </main>
   );
---
+++ src/components/sponsor-item.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface SponsorItemProps {
+  icon: string;
+  iconName: string;
+  alt: string;
+  height: number;
+  invert?: boolean;
+}
+
+function SponsorItem({
+  icon,
+  iconName,
+  alt,
+  height,
+  invert,
+}: SponsorItemProps) {
+  return (
+    <div className="flex">
+      <img
+        className={`mx-auto w-fit h-${height} mb-12 ${invert ? "invert" : ""}`}
+        src={`https://api.iconify.design/${icon}/${iconName}.svg?color=%23fff`}
+        alt={alt}
+        width="auto"
+      />
+    </div>
+  );
+}
+
+export default SponsorItem;
