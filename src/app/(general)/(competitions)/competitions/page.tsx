659ab51ae6ad568f49f7ae0e87085af1604b1378
Handthere <152689692+Handthere@users.noreply.github.com>
2025-10-18 19:11:54 +0700

    feat
    added descriptions and access to guidebook

--- src/app/(general)/(competitions)/competitions/page.tsx
+++ src/app/(general)/(competitions)/competitions/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CompetitionsSection from "./competitions";
 import { Metadata } from "next";
+import CompetitionsDesc from "./competitiondesc";
 
 export const metadata: Metadata = {
   title: "Competitions | Mechanical Festival 2026",
@@ -11,6 +12,7 @@ function CompetitionsPage() {
   return (
     <>
       <CompetitionsSection />
+      <CompetitionsDesc/>
     </>
   );
 }
