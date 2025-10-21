076efd12f81a3fb6b7c6067bcbe8c0138cdfaa2d
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-10-21 22:43:30 +0700

    fix: the issue was wrong .env variable name

--- src/app/api/payment/notification/route.ts
+++ src/app/api/payment/notification/route.ts
@@ -8,43 +8,41 @@ export async function POST(req: Request) {
   const {
     order_id,
     transaction_status,
+    transaction_id,
     signature_key,
     status_code,
     gross_amount,
   } = payload;
 
   // Verify signature received from Midtrans (from documentation must be SHA512
-  //    with order_id + status_code + gross_amount + serverKey )
-  console.log(payload);
-  const serverKey = process.env.MIDTRANS_SERVER_KEY!;
+  // with order_id + status_code + gross_amount + serverKey )
+
+  // For debugging
+  // console.log("Payload", payload);
+  // console.log("order_id: ", order_id);
+  // console.log("status_code: ", status_code);
+  // console.log("gross_amount: ", gross_amount);
+  const secretKey = process.env.MIDTRANS_SECRET_KEY as string;
+  console.log("secretKey: ", secretKey);
   const expectedSignature = crypto
     .createHash("sha512")
-    .update(order_id + status_code + gross_amount + serverKey)
+    .update(order_id + status_code + gross_amount + secretKey)
     .digest("hex");
 
+  // For debugging
+  // console.log("Signature Key: ", signature_key);
+  // console.log("Expected Signature: ", expectedSignature);
+  // console.log(
+  //   "is Expected Signature not equal to Signature Key?: ",
+  //   expectedSignature !== signature_key
+  // );
+
   if (expectedSignature !== signature_key) {
-    console.log(expectedSignature !== signature_key);
     return NextResponse.json({ message: "Invalid signature" }, { status: 403 });
   }
 
-  const res = await fetch(
-    `https://api.sandbox.midtrans.com/v2/${order_id}/status`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.MIDTRANS_SECRET_KEY}:`
-        ).toString("base64")}`,
-      },
-    }
-  );
-
-  const result = await res.json();
-
   await prisma.payment.update({
-    where: { orderId: result.order_id },
+    where: { orderId: order_id },
     data: {
       status: transaction_status,
       createdAt: new Date(),
