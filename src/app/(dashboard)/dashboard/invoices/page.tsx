885d0a378aff59b12bc19b553cde2edaaff13ad6
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-10-22 06:43:20 +0700

    style: change to smaller max width on invoice page

--- src/app/(dashboard)/dashboard/invoices/page.tsx
+++ src/app/(dashboard)/dashboard/invoices/page.tsx
@@ -16,7 +16,7 @@ async function InvoicePage() {
 
   return (
     <section className="flex min-h-screen mx-auto px-4 py-4 md:py-8">
-      <div className="bg-transparent h-fit w-full max-w-lg md:max-w-7xl overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
+      <div className="bg-transparent h-fit w-full max-w-xs md:max-w-7xl overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
         <div className="bg-transparent -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
           <h1 className="text-3xl font-bold text-foreground">Invoices</h1>
           <TableInvoices invoices={invoices} />
