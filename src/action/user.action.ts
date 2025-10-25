80a01831bc0066460ebf21425617cb3f0c632ff8
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-10-25 07:25:52 +0700

    fix: test fix

--- src/action/user.action.ts
+++ src/action/user.action.ts
@@ -6,7 +6,10 @@ import { prisma } from "@/lib/prisma";
 export async function getUserProfile() {
   const session = await auth();
   const user = await prisma.user.findUnique({
-    where: { email: session?.user.email as string },
+    where: {
+      email: session?.user.email as string,
+      id: session?.user.id as string,
+    },
   });
 
   return user;
