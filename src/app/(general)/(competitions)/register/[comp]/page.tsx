705e05ef5d14deb7a2130b987c41fb8810ef81cc
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-11-14 08:59:23 +0700

    refactor: refactor comp page

--- src/app/(general)/(competitions)/register/[comp]/page.tsx
+++ src/app/(general)/(competitions)/register/[comp]/page.tsx
@@ -6,6 +6,9 @@ import { Team, User, CompRegistration, TeamMember } from "@/types/types";
 import { competitionsName } from "@/constants/constants";
 import { Suspense } from "react";
 import { CompRegisterFormSkeleton } from "@/components/register/CompFormSkeleton";
+import Link from "next/link";
+import Image from "next/image";
+import { competitions } from "@/lib/competition";
 
 export async function generateMetadata({
   params,
@@ -27,9 +30,11 @@ export default function CompPage({
   return (
     <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
       <div className="bg-muted m-auto h-fit w-full max-w-xl verflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]">
-        <Suspense fallback={<CompRegisterFormSkeleton />}>
-          <FetchCompForm params={params} />
-        </Suspense>
+        <div className="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
+          <Suspense fallback={<CompRegisterFormSkeleton />}>
+            <FetchCompForm params={params} />
+          </Suspense>
+        </div>
       </div>
     </section>
   );
@@ -46,18 +51,6 @@ async function FetchCompForm({
     if (!competitionsName.includes(comp.toUpperCase())) {
       redirect("/competitions");
     }
-  }
-  if (
-    !user?.institution ||
-    !user?.major ||
-    !user?.education ||
-    !user?.semester ||
-    !user?.phoneNumber ||
-    !user?.domicile ||
-    !user?.birthDate ||
-    !user?.gender
-  ) {
-    redirect("/dashboard/profile?notif=incomplete_profile");
   }
   const [
     teams,
@@ -98,15 +91,52 @@ async function FetchCompForm({
     }),
   ]);
   return (
-    <RegisterForm
-      comp={comp}
-      user={user as User}
-      teams={teams as Team[]}
-      registeredCompetitions={registeredCompetitions as CompRegistration[]}
-      teamMembers={teamMembers as TeamMember[]}
-      allTeamsDatas={allTeamsDatas as Team[]}
-      allRegisteredTeamDatas={allRegisteredTeamDatas as CompRegistration[]}
-      allTeamMembersDatas={allTeamMembersDatas as TeamMember[]}
-    />
+    <>
+      <div className="text-center">
+        <Link
+          href="/"
+          aria-label="go home"
+          className="flex items-center gap-4 justify-center"
+        >
+          <Image
+            src={`/competitions/logo/${comp}.png`}
+            alt="Mechanical Festival 2026"
+            width={150}
+            height={150}
+            loading="lazy"
+          />
+        </Link>
+        <h1 className="mb-1 mt-4 text-xl font-semibold">
+          Register{" "}
+          {
+            competitions.find((c) => c.abbreviation === comp.toUpperCase())
+              ?.title
+          }
+        </h1>
+        <p className="text-sm">
+          Please fill in the form below to register for {comp.toUpperCase()}
+        </p>
+        <h2 className="text-lg text-center mt-2">
+          Fee:{" "}
+          <span className="font-bold italic">
+            Rp. {""}
+            {
+              competitions.find((c) => c.abbreviation === comp.toUpperCase())
+                ?.fee1
+            }
+          </span>
+        </h2>
+      </div>
+      <RegisterForm
+        comp={comp}
+        user={user as User}
+        teams={teams as Team[]}
+        registeredCompetitions={registeredCompetitions as CompRegistration[]}
+        teamMembers={teamMembers as TeamMember[]}
+        allTeamsDatas={allTeamsDatas as Team[]}
+        allRegisteredTeamDatas={allRegisteredTeamDatas as CompRegistration[]}
+        allTeamMembersDatas={allTeamMembersDatas as TeamMember[]}
+      />
+    </>
   );
 }
