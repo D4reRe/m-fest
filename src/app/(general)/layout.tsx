86c1cc3cd1f536b9ea53d0ef436937e7baa7ee08
Handthere <152689692+Handthere@users.noreply.github.com>
2025-11-20 14:44:11 +0700

    balikin layout

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,10 +1,11 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Navbar } from "@/components/general/Navbar";
 import FooterSection from "@/components/general/footer";
 
-
+export const metadata: Metadata = {
+  title: "Mechanical Festival 2026",
+  description: "Official website of M-Fest for events and competitions",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
