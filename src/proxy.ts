eca93504f6a6c40e9cfabab6d05b2f161d630185
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-11-29 19:06:38 +0700

    fix: added protection to admin routes in proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,7 @@ import { auth } from "@/server/auth/auth";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { adminRoles } from "@/constants/constants";
 
 export default async function proxy(request: NextRequest) {
   const session = await auth.api.getSession({
@@ -24,6 +25,14 @@ export default async function proxy(request: NextRequest) {
   }
 
   if (pathname === "/login" && session) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (
+    pathname === "/admin" &&
+    session &&
+    !adminRoles.includes(session.user.role as string)
+  ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
