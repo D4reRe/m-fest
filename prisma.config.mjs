b4140d7e1029727d9944238d066c86d0a002f6d0
Muhamad Hanif Hafizhan <147917088+EnvoyX@users.noreply.github.com>
2025-12-06 16:50:38 +0700

    fix: upgrade next & react to fix CVE-2025-55182

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
+import { env as envConfig } from "./src/env";
+
+// in development, use Supabase's direct URL, in staging (vercel) use DATABASE_URL (Neon Postgres)
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +10,9 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DIRECT_URL"),
+    url:
+      envConfig.NODE_ENV === "development"
+        ? env("DIRECT_URL")
+        : env("DATABASE_URL"),
   },
 });
--- src/app/(general)/(competitions)/competitions/CompetitionList.tsx
+++ src/app/(general)/(competitions)/competitions/CompetitionList.tsx
@@ -28,9 +28,6 @@ export default function CompetitionsList() {
             <div className="grid gap-6 md:grid-cols-2 md:gap-12">
               <h2 className="text-4xl font-medium flex flex-col gap-3">
                 {competition.title}
-                <span className="text-2xl">
-                  Registration fee: Rp.{competition.fee1}
-                </span>
               </h2>
               <div className="space-y-6">
                 <p>{competition.desc}</p>
